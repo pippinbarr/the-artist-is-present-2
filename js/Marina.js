9967e26fcb9cc324ad79cdb613c54d622f2f36b1
Pippin Barr <pippin.barr@gmail.com>
2019-09-02 16:20:54 -0400

    Marina: added a Marina class to create a Marina avatar so it's easier to incorporate into different scenes without replicating code. Imagine at some point I'll want a more generic Person class that can put together people out of a specified spritesheet and also have the option for keyboard controls? Maybe not, we'll think about that when we get there. For now it's satisfying to have more efficient code.

---
+++ js/Marina.js
@@ -0,0 +1,69 @@
+class Marina extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, texture) {
+    super(scene, x, y, texture)
+    scene.add.existing(this)
+    scene.physics.add.existing(this)
+    this.setScale(4);
+    this.body.setOffset(1,this.height-4);
+    this.body.setSize(this.width-2,4,false);
+    this.cursors = scene.input.keyboard.createCursorKeys();
+  }
+
+  create () {
+    console.log("Here?")
+  }
+
+  update () {
+    console.log("update")
+    this.handleInput();
+  }
+
+  handleInput () {
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
+      if (this.body.velocity.x < 0) {
+        this.setVelocityX(0);
+        this.anims.play('idle-h');
+      }
+      else {
+        this.flipX = true;
+        this.setVelocityX(-100);
+        this.setVelocityY(0);
+        this.anims.play('walking-h');
+      }
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
+      if (this.body.velocity.x > 0) {
+        this.setVelocityX(0);
+        this.anims.play('idle-h');
+      }
+      else {
+        this.flipX = false;
+        this.setVelocityX(100);
+        this.setVelocityY(0);
+        this.anims.play('walking-h');
+      }
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+      if (this.body.velocity.y < 0) {
+        this.setVelocityY(0);
+        this.anims.play('idle-u');
+      }
+      else {
+        this.setVelocityX(0);
+        this.setVelocityY(-100);
+        this.anims.play('walking-u');
+      }
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.down)) {
+      if (this.body.velocity.y > 0) {
+        this.setVelocityY(0);
+        this.anims.play('idle-d');
+      }
+      else {
+        this.setVelocityX(0);
+        this.setVelocityY(100);
+        this.anims.play('walking-d');
+      }
+    }
+  }
+}
--- js/Tickets.js
+++ js/Tickets.js
@@ -12,11 +12,8 @@ let Tickets = new Phaser.Class({
     // Ticket area BG
     this.add.sprite(this.game.canvas.width/2,this.game.canvas.height/2,'atlas','tickets/tickets-bg.png').setScale(4);
 
-    // Avatar (should be a class)
-    this.avatar = this.physics.add.sprite(this.game.canvas.width/2,this.game.canvas.height/2,'marina').setScale(4);
-    this.avatar.body.onWorldBounds = true;
-    this.avatar.body.setOffset(1,this.avatar.height-4);
-    this.avatar.body.setSize(this.avatar.width-2,4,false);
+    this.marina = new Marina(this,100,this.game.canvas.height/2,'marina');
+    this.marina.anims.play('idle-d');
 
     this.colliders = this.add.group();
     createColliderRect(this,0,200,600,5,this.colliders);
@@ -27,83 +24,24 @@ let Tickets = new Phaser.Class({
     createColliderRect(this,1,200,1,200,this.colliders);
     createColliderLine(this,600,200,75,80,5,5,this.colliders);
     createColliderLine(this,730,330,75,80,5,5,this.colliders);
-
     this.colliders.toggleVisible();
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.avatar.anims.play('idle-d');
-
-    this.physics.world.on('worldbounds', () => {
-      this.stopAvatar();
-    },this);
 
     let ticketFG = this.add.sprite(this.game.canvas.width/2,this.game.canvas.height/2,'atlas','tickets/tickets-fg.png').setScale(4);
     ticketFG.depth = 100000;
   },
 
-  update: function () {
-    this.handleInput();
-
-    this.physics.collide(this.avatar,this.colliders,() => {
+  update: function (time,delta) {
+    this.marina.update(time,delta);
+    this.physics.collide(this.marina,this.colliders,() => {
       this.stopAvatar();
     });
-    this.avatar.depth = this.avatar.body.y;
+    this.marina.depth = this.marina.body.y;
   },
 
   stopAvatar() {
-    let key = this.avatar.anims.currentAnim.key;
+    let key = this.marina.anims.currentAnim.key;
     key = key.replace('walking','idle');
-    this.avatar.anims.play(key);
-    this.avatar.setVelocity(0,0);
-  },
-
-  handleInput() {
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
-      if (this.avatar.body.velocity.x < 0) {
-        this.avatar.setVelocityX(0);
-        this.avatar.anims.play('idle-h');
-      }
-      else {
-        this.avatar.flipX = true;
-        this.avatar.setVelocityX(-100);
-        this.avatar.setVelocityY(0);
-        this.avatar.anims.play('walking-h');
-      }
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
-      if (this.avatar.body.velocity.x > 0) {
-        this.avatar.setVelocityX(0);
-        this.avatar.anims.play('idle-h');
-      }
-      else {
-        this.avatar.flipX = false;
-        this.avatar.setVelocityX(100);
-        this.avatar.setVelocityY(0);
-        this.avatar.anims.play('walking-h');
-      }
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
-      if (this.avatar.body.velocity.y < 0) {
-        this.avatar.setVelocityY(0);
-        this.avatar.anims.play('idle-u');
-      }
-      else {
-        this.avatar.setVelocityX(0);
-        this.avatar.setVelocityY(-100);
-        this.avatar.anims.play('walking-u');
-      }
-    }
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.down)) {
-      if (this.avatar.body.velocity.y > 0) {
-        this.avatar.setVelocityY(0);
-        this.avatar.anims.play('idle-d');
-      }
-      else {
-        this.avatar.setVelocityX(0);
-        this.avatar.setVelocityY(100);
-        this.avatar.anims.play('walking-d');
-      }
-    }
-  },
+    this.marina.anims.play(key);
+    this.marina.setVelocity(0,0);
+  }
 });

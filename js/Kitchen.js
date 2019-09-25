95af9fda5a32689b7359a621b2c6d88550d5fe1f
Pippin Barr <pippin.barr@gmail.com>
2019-09-25 15:51:29 -0400

    Kitchen: fixed image process a bit, added all colliders

--- js/Kitchen.js
+++ js/Kitchen.js
@@ -16,45 +16,45 @@ let Kitchen = new Phaser.Class({
     // Room
     this.add.sprite(this.game.canvas.width / 2, this.game.canvas.height / 2, 'atlas', 'kitchen/kitchen-bg.png').setScale(4);
     // Wall colliders
-    // createColliderRect(this, 57 * 4, 56 * 4, (140 - 57) * 4, 5, this.colliders);
+    createColliderLine(this, 43 * 4, 76 * 4, 22 * 4, 22 * 4, -5, 5, this.colliders);
+    createColliderLine(this, 57 * 4, 60 * 4, 4 * 4, 4 * 4, 5, -5, this.colliders);
+    createColliderRect(this, 61 * 4, 56 * 4, 122 * 4, 1 * 4, this.colliders);
+    createColliderRect(this, 0 * 4, 60 * 4, 58 * 4, 1 * 4, this.colliders);
+    createColliderRect(this, 0 * 4, 76 * 4, 44 * 4, 1 * 4, this.colliders);
+    createColliderRect(this, 0 * 4, 99 * 4, 200 * 4, 1 * 4, this.colliders);
+    createColliderRect(this, 164 * 4, 56 * 4, 1 * 4, 27 * 4, this.colliders);
+
+
     // createColliderRect(this, 0 * 4, 92 * 4, 200 * 4, 5, this.colliders);
     // createColliderRect(this, 151 * 4, 66 * 4, 200 * 4, 5, this.colliders);
-    // createColliderLine(this, 140 * 4, 56 * 4, 10 * 4, 11 * 4, 5, 5, this.colliders);
     // createColliderLine(this, 164 * 4, 82 * 4, 10 * 4, 11 * 4, 5, 5, this.colliders);
     // createColliderLine(this, 57 * 4, 57 * 4, 35 * 4, 35 * 4, -5, 5, this.colliders);
 
-    // Kitchen Island
-    this.kitchenIslandBG = this.physics.add.sprite(129 * 4, 36 * 4, 'atlas', 'kitchen/kitchen-island-bg.png').setScale(4);
-    this.kitchenIslandBG.body.setOffset(0, this.kitchenIslandBG.body.height - 16);
-    this.kitchenIslandBG.body.setSize(this.kitchenIslandBG.width, 16, false);
-    this.kitchenIslandBG.body.immovable = true;
+    // Kitchen Unit BG
+    this.unitBG = this.physics.add.sprite(this.game.canvas.width / 2, this.game.canvas.height / 2, 'atlas', 'kitchen/unit-bg.png').setScale(4);
+    this.unitBG.body.setOffset(76, 54);
+    this.unitBG.body.setSize(106, 6, false);
+    this.unitBG.body.immovable = true;
+    this.colliders.add(this.unitBG)
 
-    this.kitchenIslandFG = this.physics.add.sprite(this.game.canvas.width / 2, 41 * 4, 'atlas', 'kitchen/kitchen-island-fg.png').setScale(4);
-    this.kitchenIslandFG.body.setOffset(0, this.kitchenIslandFG.body.height - 16);
-    this.kitchenIslandFG.body.setSize(this.kitchenIslandFG.width, 16, false);
-    this.kitchenIslandFG.body.immovable = true;
-
-    // Bed colliders
-    // createColliderRect(this, 32 * 4, 68 * 4, 70 * 4, 5, this.colliders);
-    // createColliderRect(this, 31 * 4, 83 * 4, 57 * 4, 5, this.colliders);
-    // createColliderLine(this, 102 * 4, 69 * 4, 16 * 4, 16 * 4, -5, 5, this.colliders);
+    // Kitchen Unit FG
+    this.unitFG = this.physics.add.sprite(this.game.canvas.width / 2, this.game.canvas.height / 2, 'atlas', 'kitchen/unit-fg.png').setScale(4);
+    this.unitFG.body.setOffset(76, 76);
+    this.unitFG.body.setSize(106, 6, false);
+    this.unitFG.body.immovable = true;
+    this.unitFG.depth = 76 * 4;
+    this.colliders.add(this.unitFG);
 
     // Marina Abramovic
     this.marina = new Marina(this, 280, 290, 'marina');
     this.marina.anims.play('idle-d');
 
-    this.colliders.toggleVisible();
+    // this.colliders.toggleVisible();
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    let kitchenFG = this.physics.add.sprite(35 * 4, 48 * 4, 'atlas', 'kitchen/kitchen-fg.png').setScale(4);
-    kitchenFG.depth = kitchenFG.y + kitchenFG.height + 10 * 4;
-
-    this.depthGroup = this.add.group();
-    this.depthGroup.add(this.marina);
-    this.depthGroup.add(this.kitchenIslandBG);
-    this.depthGroup.add(this.kitchenIslandFG);
-    this.depthGroup.add(kitchenFG);
+    let kitchenFG = this.add.sprite(this.game.canvas.width / 2, this.game.canvas.height / 2, 'atlas', 'kitchen/kitchen-fg.png').setScale(4);
+    kitchenFG.depth = 72 * 4;
   },
 
   update: function(time, delta) {
@@ -63,8 +63,6 @@ let Kitchen = new Phaser.Class({
       this.marina.stop();
     });
     this.marina.depth = this.marina.body.y;
-    this.kitchenIslandBG.depth = this.kitchenIslandBG.body.y;
-    this.kitchenIslandFG.depth = this.kitchenIslandFG.body.y;
   },
 
 });
--- js/helpers.js
+++ js/helpers.js
@@ -1,19 +1,20 @@
-function createColliderRect (self,x,y,width,height,group) {
-  let p = self.physics.add.sprite(x,y,'atlas','red-pixel.png');
-  p.setOrigin(0,0);
-  p.setScale(width,height);
+function createColliderRect(self, x, y, width, height, group) {
+  let p = self.physics.add.sprite(x, y, 'atlas', 'red-pixel.png');
+  p.setOrigin(0, 0);
+  p.visible = false;
+  p.setScale(width, height);
   p.body.immovable = true;
   group.add(p);
 }
 
-function createColliderLine (self,fromX,fromY,distX,distY,dX,dY,group) {
+function createColliderLine(self, fromX, fromY, distX, distY, dX, dY, group) {
   // we need a diagonal, so we'll make it out of dots
   let x = 0;
   let y = 0;
   while (Math.abs(x) <= distX && Math.abs(y) <= distY) {
-    let p = self.physics.add.sprite(fromX + x,fromY + y,'atlas','red-pixel.png');
-    p.setOrigin(0,0);
-    p.setScale(1,1);
+    let p = self.physics.add.sprite(fromX + x, fromY + y, 'atlas', 'red-pixel.png');
+    p.setOrigin(0, 0);
+    p.setScale(1, 1);
     p.body.immovable = true;
     group.add(p);
     x += dX;

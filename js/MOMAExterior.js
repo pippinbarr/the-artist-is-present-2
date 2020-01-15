6ff732a07bbacd9ce0640995f605567aed716516
Pippin Barr <pippin.barr@gmail.com>
2020-01-15 15:07:42 -0500

    Added wrapping and bottom limit

--- js/MOMAExterior.js
+++ js/MOMAExterior.js
@@ -19,6 +19,7 @@ class MOMAExterior extends TAIPScene {
     this.RIGHT_DOOR_X = this.game.canvas.width / 2 + 10.5 * 4;
     this.DOOR_OPEN_AMOUNT = 15 * 4;
 
+    this.doorsOpen = false;
     this.leftDoor = this.physics.add.sprite(this.LEFT_DOOR_X, this.game.canvas.height / 2 - 2.5 * 4, 'atlas', 'moma-exterior/moma-door.png').setScale(4).setDepth(-10);
     this.rightDoor = this.physics.add.sprite(this.RIGHT_DOOR_X, this.game.canvas.height / 2 - 2.5 * 4, 'atlas', 'moma-exterior/moma-door.png').setScale(4).setDepth(-10);
     this.leftDoor.body.immovable = true;
@@ -36,8 +37,10 @@ class MOMAExterior extends TAIPScene {
     createColliderRect(this, 0 * 4, 0 * 4, 32 * 4, 85 * 4, this.colliders);
     createColliderRect(this, 168 * 4, 0 * 4, 32 * 4, 85 * 4, this.colliders);
     // Diagonal walls
-    createColliderLine(this, 32 * 4, 85 * 4, 20 * 4, 20 * 4, 5, -5, this.colliders);
+    createColliderLine(this, 32 * 4, 84 * 4, 20 * 4, 20 * 4, 5, -5, this.colliders);
     createColliderLine(this, 147 * 4, 64 * 4, 20 * 4, 20 * 4, 5, 5, this.colliders);
+    // Bottom wall
+    createColliderRect(this, 0 * 4, 98 * 4, 200 * 4, 2 * 4, this.colliders);
 
 
     // Marina Abramovic
@@ -63,6 +66,21 @@ class MOMAExterior extends TAIPScene {
     this.physics.collide(this.marina, this.colliders, () => {
       this.marina.stop();
     });
+    this.handleWrap();
+    this.handleSensor();
+    this.marina.depth = this.marina.body.y;
+  }
+
+  handleWrap() {
+    if (this.marina.x < 0 - this.marina.width) {
+      this.marina.x = this.game.canvas.width + this.marina.width;
+    }
+    else if (this.marina.x > this.game.canvas.width + this.marina.width) {
+      this.marina.x = 0 - this.marina.width;
+    }
+  }
+
+  handleSensor() {
     if (this.physics.overlap(this.sensor, this.marina)) {
       if (!this.sensor.activated) {
         this.sensor.activated = true;
@@ -80,11 +98,13 @@ class MOMAExterior extends TAIPScene {
           x: this.RIGHT_DOOR_X + this.DOOR_OPEN_AMOUNT,
           duration: 750,
           repeat: 0,
-          onComplete: () => {},
+          onComplete: () => {
+            this.doorsOpen = true;
+          },
         });
       }
     }
-    else if (this.sensor.activated) {
+    else if (this.sensor.activated && this.doorsOpen) {
       this.sensor.activated = false;
       const leftDoorTween = this.tweens.add({
         targets: this.leftDoor,
@@ -100,9 +120,10 @@ class MOMAExterior extends TAIPScene {
         x: this.RIGHT_DOOR_X,
         duration: 750,
         repeat: 0,
-        onComplete: () => {},
+        onComplete: () => {
+          this.doorsOpen = false;
+        },
       });
     }
-    this.marina.depth = this.marina.body.y;
   }
 }
